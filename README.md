df44c6b66c9b02624050614d02ecfb3b3331d902
Ben Olney <84375142+Olney1@users.noreply.github.com>
2023-01-07 12:24:11 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Speech recognition and text-to-speech using OpenAI and gTTS
+
+This script allows you to use speech recognition to input a prompt, send the prompt to OpenAI to generate a response, and then use gTTS to convert the response to an audio file and play the audio file.
+
+## Prerequisites
+
+- You need to have a valid OpenAI API key. You can sign up for a free API key at https://beta.openai.com/.
+- You need to install the following packages: `openai`, `gTTS`, `pyaudio`, `speech_recognition`, `playsound`. You can install these packages using `pip install openai gTTS pyaudio speech_recognition playsound`.
+
+## Usage
+
+1. Replace `YOUR_API_KEY_HERE` in the following line with your actual OpenAI API key: `openai.api_key = "YOUR_API_KEY_HERE"`
+
+2. Run the script using `python script.py`.
+3. The script will prompt you to say something. Speak a sentence into your microphone.
+4. The script will send the spoken sentence to OpenAI, generate a response using the text-to-speech model, and play the response as an audio file.
+
+## Customization
+
+- You can change the OpenAI model engine by modifying the value of `model_engine`. For example, to use the "davinci" engine, set `model_engine = "davinci"`.
+- You can change the language of the generated audio file by modifying the value of `language`. For example, to generate audio in French, set `language = 'fr'`.
+- You can adjust the `temperature` parameter in the following line to control the randomness of the generated response:
+
+```
+completions = openai.Completion.create(
+engine=model_engine,
+prompt=prompt,
+max_tokens=1024,
+n=1,
+temperature=0.7,
+)
+```
+
+Higher values of `temperature` will result in more diverse and random responses, while lower values will result in more deterministic responses.
---
+++ smart_speaker.py
@@ -0,0 +1,61 @@
+import openai
+import os
+import pyaudio
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+openai.api_key = "YOUR-KEY-GOES-HERE"
+model_engine = "text-davinci-002"
+language = 'en'
+
+def recognize_speech():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        speech = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    return speech
+
+def get_completions(prompt):
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        temperature=0.7,
+    )
+    return completions
+
+def generate_audio_file(text):
+    myobj = gTTS(text=text, lang=language, slow=False)
+    myobj.save("response.mp3")
+
+def play_audio_file():
+    os.system("mpg321 response.mp3")
+    playsound("response.mp3", block=False) # There’s an optional second argument, block, which is set to True by default. Setting it to False makes the function run asynchronously.
+
+def main():
+    prompt = recognize_speech()
+    print(f"This is the prompt being sent to OpenAI" + prompt)
+    completions = get_completions(prompt)
+    message = completions.choices[0].text
+    print(message)
+    generate_audio_file(message)
+    play_audio_file()
+
+if __name__ == "__main__":
+    main()
