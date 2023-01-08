4e01d00fc128c3d9e8b27f3584f4524459923efd
Ben Olney <84375142+Olney1@users.noreply.github.com>
2023-01-08 19:16:30 +0000

    Update smart_speaker.py
    comments added to explain more about each function.

--- smart_speaker.py
+++ smart_speaker.py
@@ -5,6 +5,7 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
+# settings and keys
 openai.api_key = "YOUR_API_KEY_HERE"
 model_engine = "text-davinci-002"
 language = 'en'
@@ -21,6 +22,7 @@ def recognize_speech():
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
+        # convert the audio to text
         print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
         speech = r.recognize_google(audio)
     except sr.UnknownValueError:
@@ -31,6 +33,7 @@ def recognize_speech():
     return speech
 
 def chatgpt_response(prompt):
+    # send the converted audio text to chatgpt
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
@@ -41,14 +44,18 @@ def chatgpt_response(prompt):
     return response
 
 def generate_audio_file(text):
+    # convert and save the text response from chatgpt to an audio file 
     myobj = gTTS(text=text, lang=language, slow=False)
+    # save the audio file
     myobj.save("response.mp3")
 
 def play_audio_file():
+    # play the audio file
     os.system("mpg321 response.mp3")
     playsound("response.mp3", block=False) # There’s an optional second argument, block, which is set to True by default. Setting it to False makes the function run asynchronously.
 
 def main():
+    # run the program
     prompt = recognize_speech()
     print(f"This is the prompt being sent to OpenAI" + prompt)
     responses = chatgpt_response(prompt)
